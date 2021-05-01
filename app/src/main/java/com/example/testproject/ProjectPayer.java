8dbff49401d4ef810c290f37043fc628cb36d829
Andrew-1000 <andruambia95@gmail.com>
2021-05-01 15:01:27 +0300

    Firebase authentication for user logout

--- app/src/main/java/com/example/testproject/ProjectPayer.java
+++ app/src/main/java/com/example/testproject/ProjectPayer.java
@@ -5,6 +5,8 @@ import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -12,19 +14,45 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.testproject.utils.Tools;
-
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class ProjectPayer extends AppCompatActivity {
 
     private Toolbar toolbar;
 
+    private Button mLogout;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project_payer);
+
+        mLogout = findViewById(R.id.action_logout);
+        mAuth = FirebaseAuth.getInstance();
+
+        mLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                startActivity(new Intent(ProjectPayer.this, SimpleStart.class));
+                finish();
+            }
+        });
+
         initToolbar();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser == null) {
+            startActivity(new Intent(this, SimpleStart.class));
+            finish();
+        }
     }
 
     private void initToolbar() {
