b0fad23fe583bef1844614267de7ef5a94dd268c
Andrew-1000 <andruambia95@gmail.com>
2021-05-01 20:11:00 +0300

    Dependencies for Firebase core introduced for PhoneAuthOptions Builder

--- app/src/main/java/com/example/testproject/SimpleStart.java
+++ app/src/main/java/com/example/testproject/SimpleStart.java
@@ -1,21 +1,41 @@
 package com.example.testproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.Toast;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class SimpleStart extends AppCompatActivity {
     private ProgressBar progressBar;
     private FloatingActionButton fab;
     private View parent_view;
+
+    private TextView processOtp;
+
+    private EditText countryCode, phoneNumber;
+    private FirebaseAuth auth;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallBacks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +44,10 @@ public class SimpleStart extends AppCompatActivity {
         parent_view = findViewById(android.R.id.content);
         progressBar = findViewById(R.id.progress_bar);
         fab = findViewById(R.id.fab);
+        processOtp = findViewById(R.id.processOtp);
+        countryCode = findViewById(R.id.countryCode);
+        phoneNumber = findViewById(R.id.phoneNumber);
+        auth = FirebaseAuth.getInstance();
 
         ((View) findViewById(R.id.sign_up_for_account)).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,20 +59,62 @@ public class SimpleStart extends AppCompatActivity {
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v)  {
                 findAction();
             }
         });
     }
 
-    private void findAction() {
-        Intent twoFactor = new Intent(this, Verification.class);
-        twoFactor.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = auth.getCurrentUser();
+        if (user !=null) {
+            sendtoMain();
+        }
+    }
 
+    private void sendtoMain() {
+
+        Intent mainIntent = new Intent(SimpleStart.this, ProjectPayer.class);
+        startActivity(mainIntent);
+        finish();
+    }
+    private void signIn(PhoneAuthCredential credential) {
+        auth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                 if (task.isSuccessful()) {
+                     sendtoMain();
+                 } else {
+                     processOtp.setText(task.getException().getMessage());
+                     processOtp.setTextColor(Color.WHITE);
+                     processOtp.setVisibility(View.VISIBLE);
+                 }
+            }
+        });
+    }
+
+    private void findAction() {
+        String country_code = countryCode.getText().toString().trim();
+        String phone_number = phoneNumber.getText().toString().trim();
+        String pNumber = "+" + country_code + "" + phone_number;
+        if (!country_code.isEmpty() || !phone_number.isEmpty()) {
+            PhoneAuthOptions options = PhoneAuthOptions.newBuilder(auth)
+                    .setPhoneNumber(pNumber)
+                    .setTimeout(60L, TimeUnit.SECONDS)
+                    .setActivity(SimpleStart.this)
+                    .setCallbacks(mCallBacks)
+                    .build();
+            PhoneAuthProvider.verifyPhoneNumber(options);
+        } else
+        {
+            processOtp.setText("Please enter Country code and Phone Number");
+            processOtp.setTextColor(Color.WHITE);
+            processOtp.setVisibility(View.VISIBLE);
+        }
         progressBar.setVisibility(View.VISIBLE);
         fab.setAlpha(0f);
-        startActivity(twoFactor);
-        Toast.makeText(this, "Code sent to your Phone", Toast.LENGTH_LONG).show();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -57,7 +123,35 @@ public class SimpleStart extends AppCompatActivity {
 
             }
         }, 1000);
+mCallBacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+    @Override
+    public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+        signIn(phoneAuthCredential);
+    }
 
+    @Override
+    public void onVerificationFailed(@NonNull FirebaseException e) {
+        processOtp.setText(e.getMessage());
+        processOtp.setTextColor(Color.RED);
+        processOtp.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+        super.onCodeSent(s, forceResendingToken);
+        processOtp.setText("OTP has been sent");
+        processOtp.setText(View.VISIBLE);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent otpIntent = new Intent(SimpleStart.this, ProjectPayer.class);
+                otpIntent.putExtra("auth", s);
+                startActivity(otpIntent);
+            }
+        }, 10000);
+
+    }
+};
 
     }
 }
